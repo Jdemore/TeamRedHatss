e6e66a1c7deb344335e9bcf8d33ffa604e3e2122
Jdemore <Jdemore4@kent.edu>
2026-03-28 13:23:00 -0400

    ~

--- init-lfs.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: ============================================
-:: init-lfs.bat
-:: Double-click this to run init-lfs.ps1
-:: ============================================
-powershell -ExecutionPolicy Bypass -File "%~dp0init-lfs.ps1"
-pause
